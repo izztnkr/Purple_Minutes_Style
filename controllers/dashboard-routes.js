5ad60472dbe6ab75cd85c42f20f83778db551773
Dakota Donaldson <dakota.donaldson17@icloud.com>
2021-11-18 00:32:53 -0600

    created new handlebars files for dashboard navigation

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const withAuth = require('../utils/auth');
 
 
-router.get('/', withAuth, (req,res) => {
+
+router.get('/' , (req,res) => {
     res.render('dashboard');
 
 });
