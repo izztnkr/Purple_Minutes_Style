0a1e73d4775a1364399fbcb5fbe22b47c18dd95c
Izzy Ramirez <izzcreativa@gmail.com>
2021-11-18 21:58:40 -0600

    Update login.js

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -7,7 +7,7 @@ async function loginEvent(event){
     const password = document.querySelector('#password-login').value.trim();
 
     if (username && password){
-        const response = await fetch("/login", {
+        const response = await fetch("/api/users/login", {
             method: 'post',
             body: JSON.stringify({
                 username,
