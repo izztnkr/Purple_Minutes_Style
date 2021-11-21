ad592420e99d4c443f48be24ba7a2840044c9d4a
Ethan Chandler <86173119+IzzTinkr@users.noreply.github.com>
2021-11-21 13:17:46 -0600

    troubleshooting deployment

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -143,4 +143,18 @@ router.get("/editorial", (req, res) => {
       res.status(500).json(err);
     });
 });
+
+//create logout
+// router.post('/logout', (req,res) => {
+
+//   if (req.session.loggedIn){
+//       req.session.destroy(() => {
+//           res.status(204).end();
+//       });
+//   } else{
+//       res.status(404).end();
+//   }
+
+// });
+
 module.exports = router;
